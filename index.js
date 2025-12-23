95c61675f8f6fee2426bc3149eeae89e47cc5dec
Abigael Ongwae <abbykerry17@gmail.com>
2025-12-23 09:14:16 +0300

    modified index.js and index.test.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here!
+function addItem() {
+    //selct the <ul> element by its ID
+    const list = document.getElementById("item-list");
+    //create a new <li> element
+    const newItem = document.createElement("li");
+    //add text content inside the list
+    newItem.textContent= "New Item";
+    //append the new <li> to the <ul>
+    list.appendChild(newItem);
+}
+//Add a click event listener to the button with the ID 'add-item' after DOMContentLoaded
+// When clicked, it will call the addItem function
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("add-item").addEventListener("click", addItem);
+})
